50262712862d388bc7e0e3844c664c4b3cf31a5a
Li Luo <lluo@thoughtworks.com>
2018-04-20 09:58:40 +0800

    [android] - add mtk LinkIt_SDK_V4.6.1_public jni in

--- android/src/main/java/com/luoli/smartconnection/RNSmartconnectionModule.java
+++ android/src/main/java/com/luoli/smartconnection/RNSmartconnectionModule.java
@@ -1,6 +1,9 @@
 
 package com.luoli.smartconnection;
 
+import android.util.Log;
+
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -18,5 +21,43 @@ public class RNSmartconnectionModule extends ReactContextBaseJavaModule {
   @Override
   public String getName() {
     return "RNSmartconnection";
+  }
+
+  @ReactMethod
+  public void startConnection(String key, String target, Integer version, Float oldInterval, Float newInterval,
+                              Promise promise) {
+    try {
+      SmartConfigHelper.init();
+      int libVersion = SmartConfigHelper.GetLibVersion();
+      int protoVersion = SmartConfigHelper.GetProtoVersion();
+      Log.i("Smartconfig", "libVersion: " + libVersion + ", protoVersion: " + protoVersion);
+      SmartConfigHelper.initConnection(key, target, version);
+      SmartConfigHelper.setSendInterval(oldInterval, newInterval);
+      promise.resolve(System.currentTimeMillis());
+    } catch (Exception e) {
+      promise.reject(e.getCause());
+    }
+  }
+
+  @ReactMethod
+  public void sendConfiguration(String ssid, String ssidpwd, String authcode,
+                                Promise promise) {
+    try {
+      SmartConfigHelper.startConnection(ssid, ssidpwd, authcode);
+      promise.resolve(System.currentTimeMillis());
+    } catch (Exception e) {
+      e.printStackTrace();
+      promise.reject(e.getCause());
+    }
+  }
+
+  @ReactMethod
+  public void stopConnection() {
+    try {
+      SmartConfigHelper.stopConnection();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
   }
 }
---
+++ android/src/main/java/com/luoli/smartconnection/SmartConfigHelper.java
@@ -0,0 +1,53 @@
+package com.luoli.smartconnection;
+
+public class SmartConfigHelper {
+
+    private static JniLoader jniLoader;
+    private static boolean libLoaded = false;
+
+    public static void init() {
+        if (!libLoaded) {
+            libLoaded = JniLoader.LoadLib();
+        }
+        if (libLoaded) {
+            jniLoader = new JniLoader();
+        }
+    }
+
+    public static int GetProtoVersion() {
+        return jniLoader.GetProtoVersion();
+    }
+
+    public static int GetLibVersion() {
+        return jniLoader.GetLibVersion();
+    }
+
+    public static int initConnection(String key, String target, int version) {
+        int sendV1 = 0;
+        int sendV4 = 1;
+        int sendV5 = 0;
+
+        if (version == 1) {
+            sendV1 = 1;
+            sendV4 = 0;
+        }
+        if (version == 5) {
+            sendV4 = 0;
+            sendV5 = 1;
+        }
+
+        return jniLoader.InitSmartConnection(key, target, sendV1, sendV4, sendV5);
+    }
+
+    public static int setSendInterval(float oldInterval, float newInterval) {
+        return jniLoader.SetSendInterval(oldInterval, newInterval);
+    }
+
+    public static int startConnection(String SSID, String Password, String Custom) {
+        return jniLoader.StartSmartConnection(SSID, Password, Custom);
+    }
+
+    public static int stopConnection() {
+        return jniLoader.StopSmartConnection();
+    }
+}
