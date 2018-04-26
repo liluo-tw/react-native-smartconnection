9c6a99386d5790ee85245f111d6df083dcf7e1e0
Zhang Shuai <520zhangshuai@gmail.com>
2018-04-26 17:55:40 +0800

    add rct export method in ios

--- ios/RNSmartconnection.m
+++ ios/RNSmartconnection.m
@@ -1,5 +1,6 @@
 
 #import "RNSmartconnection.h"
+#import <React/RCTLog.h>
 
 @implementation RNSmartconnection
 
@@ -7,7 +8,37 @@
 {
     return dispatch_get_main_queue();
 }
-RCT_EXPORT_MODULE()
+
+RCT_EXPORT_MODULE(@"RNSmartconnection");
+
+RCT_EXPORT_METHOD(startConnection: (NSString *)key
+                 target: (NSString *)target
+                 version: (NSNumber *)version
+                 oldInterval: (float)oldInterval
+                 newInterval: (float)newInterval
+                 resolver: (RCTPromiseResolveBlock)resolve
+                 rejecter: (RCTPromiseRejectBlock)reject)
+{
+    RCTLogInfo(@"startConnection %@ %@ %d %f %f", key, target, version, oldInterval, newInterval);
+    resolve([NSNumber numberWithInt:1]);
+}
+
+RCT_EXPORT_METHOD(sendConfiguration: (NSString *)ssid
+                 pwd: (NSString *)pwd
+                 authcode: (NSString *)authcode
+                 resolver: (RCTPromiseResolveBlock)resolve
+                 rejecter: (RCTPromiseRejectBlock)reject)
+{
+    RCTLogInfo(@"sendConfiguration %@ %@ %@", ssid, pwd, authcode);
+    resolve(@"");
+}
+
+RCT_EXPORT_METHOD(stopConnection: (RCTPromiseResolveBlock)resolve
+                 rejecter: (RCTPromiseRejectBlock)reject)
+{
+    RCTLogInfo(@"Stop smart connection");
+    resolve(@"");
+}
 
 @end
   
